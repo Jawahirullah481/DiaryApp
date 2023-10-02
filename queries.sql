9dd410518d62269c182e09223bb514d9a020b7c4
Jawahirullah481 <jawahirullah481@gmail.com>
2023-10-02 23:26:58 +0530

    Queries for this Application

---
+++ queries.sql
@@ -0,0 +1,51 @@
+-- Creating diaryapp database--
+create database diaryappclone;
+
+
+-- selecting the database--
+use diaryappclone;
+
+
+-- Creating diaryappuser table --
+create table diaryappuser(
+userId Int NOT NULL AUTO_INCREMENT,
+Username varchar(25) NOT NULL,
+EmailId varchar(25) NOT NULL,
+Password varchar(20) NOT NULL,
+PRIMARY KEY (userId)
+);
+
+
+-- Creating diaryappinbox table --
+create table diaryappinbox(
+Id int NOT NULL AUTO_INCREMENT,
+Heading varchar(35) NOT NULL,
+Date date,
+Content varchar(1500),
+userId int,
+PRIMARY KEY(id),
+FOREIGN KEY(UserId) references diaryappuser(userId)
+);
+
+
+-- Creating stored procedure for deleteUser --
+DELIMITER $$
+CREATE PROCEDURE `deleteUser`(IN i INT)
+BEGIN
+SET FOREIGN_KEY_CHECKS=0;
+delete from diaryappuser where userid = i;
+delete from diaryappinbox where userid = i;
+SET FOREIGN_KEY_CHECKS=1;
+END$$
+DELIMITER ;
+
+
+-- Creating stored procedure for edit diary in inbox --
+DELIMITER $$
+CREATE PROCEDURE `editInbox`(IN newheading VARCHAR(25), IN newcontent VARCHAR(1500), IN newdate DATE, INOUT inboxid INT, IN userid INT)
+BEGIN
+delete from diaryappinbox where id = inboxid;
+insert into diaryappinbox (heading, date, content, userid) values(newheading, newdate, newcontent, userId);
+select inbox.id into inboxid from diaryappinbox as inbox where inbox.userId = userid order by inbox.id desc limit 1;
+END$$
+DELIMITER ;
